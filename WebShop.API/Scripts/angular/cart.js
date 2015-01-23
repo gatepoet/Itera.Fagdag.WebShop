f3caeca390142e9e6da1b91b7be290197704acbb
kjartanp <kjartan_palsson@hotmail.com>
2015-01-23 15:29:18 +0100

    Cart angular

---
+++ WebShop.API/Scripts/angular/cart.js
@@ -0,0 +1,11 @@
+﻿var shoebalooApp = angular.module('shoebalooWeb');
+
+shoebalooApp.controller('CartController', ['$scope', 'productFactory', function ($scope, productFactory) {
+    function init() {
+        productFactory.getCart().success(function (data) {
+            $scope.products = data;
+        });
+    }
+
+    init();
+}]);
